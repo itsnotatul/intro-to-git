c9a4f21235cb482b8aa33cc67eb36e0fb0f3f9f6
root <atultiwariee60@gmail.com>
2020-04-05 01:01:23 +0000

    this is final version now

--- v5.js
+++ v5.js
@@ -18,7 +18,8 @@ var commentRoutes    = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes      = require("./routes/index");
 
- mongoose.connect(process.env.DATABASEURL,{
+var url = process.env.DATABASEURL ||  "mongodb://localhost/yelp_camp_v5_finalfinal"
+mongoose.connect(url,{
 	useNewUrlParser:true,
 	useCreateIndex:true
 });
